9bcd893c3d8441f70bf7b7d93aefccdeff8bc1eb
Ilaria Roglieri <ilariaroglieri@gmail.com>
2026-02-19 18:20:12 +0100

    setting up acf fields

--- functions.php
+++ functions.php
@@ -13,7 +13,6 @@ function register_my_menu() {
 add_action( 'init', 'register_my_menu' );
 
 
-
 // enqueue scripts
 function jquery_scripts() {
 	wp_enqueue_script( 'custom', get_stylesheet_directory_uri() . '/assets/js/custom.js', array(), '1.0.0', true );
@@ -28,55 +27,229 @@ function register_theme_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'register_theme_styles' );
 
-add_theme_support( 'post-thumbnails' ); 
 
-// remove_filter('the_content', 'wpautop');
 
-//Page Slug Body Class
-function add_slug_body_class( $classes ) {
-	global $post;
-	if ( isset( $post ) ) {
-		$classes[] = $post->post_type . '-' . $post->post_name;
-	}
-	return $classes;
+add_theme_support('post-thumbnails');
+
+add_image_size('thumbnail', 100, 0, false);
+
+add_image_size('full-width', 2560, 0, false);
+add_image_size('full-width-mobile', 768, 0, false);
+
+add_image_size('grid-10', 2100, 0, false);
+add_image_size('grid-6', 1300, 0, false);
+add_image_size('grid-4', 900, 0, false);
+
+
+
+
+// rename default post type
+function rename_default_post_label() {
+  global $menu;
+  global $submenu;
+  $menu[5][0] = 'Projects';
+  $submenu['edit.php'][5][0] = 'Projects';
+  $submenu['edit.php'][10][0] = 'Add Project';
+  $submenu['edit.php'][16][0] = 'Projects Tags';
 }
-add_filter( 'body_class', 'add_slug_body_class' );
+function rename_default_post_object() {
+  global $wp_post_types;
+  $labels = &$wp_post_types['post']->labels;
+  $labels->name = 'Projects';
+  $labels->singular_name = 'Project';
+  $labels->add_new = 'Add Project';
+  $labels->add_new_item = 'Add Project';
+  $labels->edit_item = 'Edit Project';
+  $labels->new_item = 'Projects';
+  $labels->view_item = 'View Projects';
+  $labels->search_items = 'Search Projects';
+  $labels->not_found = 'No Projects found';
+  $labels->not_found_in_trash = 'No Projects found in Trash';
+  $labels->all_items = 'All Projects';
+  $labels->menu_name = 'Projects';
+  $labels->name_admin_bar = 'Projects';
+}
+ 
+add_action( 'admin_menu', 'rename_default_post_label' );
+add_action( 'init', 'rename_default_post_object' );
+
 
 // img attachment defaults
+function grid_image($image_id, $cols, $context = 'default') {
+  $meta = wp_get_attachment_metadata($image_id);
 
-function default_attachment_display_settings() {
-    update_option( 'image_default_link_type', 'none' );
-    update_option( 'image_default_size', 'full' );
-}
+  if (!$meta) {
+    return '';
+  }
 
-add_action( 'after_setup_theme', 'default_attachment_display_settings' );
+  $width  = $meta['width'];
+  $height = $meta['height'];
 
-// options page
-if( function_exists('acf_add_options_page') ) {
-  acf_add_options_page(array(
-    'page_title'  => 'Settings',
-    'menu_title'  => 'Settings',
-    'menu_slug'   => 'settings',
-    'capability'  => 'edit_posts',
-    'redirect'    => false
-  ));
-}
+  $is_vertical = $height > $width;
 
-add_action('admin_head', 'my_custom_wysiwyg');
-function my_custom_wysiwyg() {
-  echo '<style>
-    .acf-editor-wrap iframe {
-      min-height: 100px !important;
-      height: 100px;
-    } 
-    .acf-gallery {
-    	height: 200px !important;
+
+  // in homepage le verticali si rimpicciliscono
+  if ($context === 'homepage' && $is_vertical) {
+
+    if ($cols == 6) {
+      $cols = 5;
     }
-    .wp-editor-area {
-    	height: 70px !important;
+
+    if ($cols == 4) {
+      $cols = 3;
     }
-  </style>';
+  }
+
+  $size_map = [
+    12 => 'full-width',
+    10 => 'd-ten-twelfth',
+    9  => 'd-ten-twelfth',
+    6  => 'd-half',
+    5  => 'd-half',   // usa stessa size, cambia solo sizes
+    4  => 'd-one-third',
+    3  => 'd-one-third',   // idem
+  ];
+
+  $size = $size_map[$cols] ?? 'grid-6';
+
+  //Calcolo percentuale viewport
+  $percentage = ($cols / 12) * 100;
+
+  $sizes = "(max-width: 768px) 100vw, {$percentage}vw";
+
+  return wp_get_attachment_image(
+    $image_id,
+    $size,
+    false,
+    [
+      'class' => 'project_image',
+      'sizes' => $sizes,
+    ]
+  );
 }
+
+
+// add acf column to projects admin page
+function add_acf_columns($columns) {
+  $columns['size'] = 'Size';
+  return $columns;
+}
+
+function featured_size_column($column, $post_id) {
+  if ( $column == 'size' ) {
+    $featured_size_field = get_field_object('featured_medium_size', $post_id);
+
+    if ($featured_size_field) {
+      $featured_size_value = get_field('featured_medium_size', $post_id);
+      $featured_size_label = $featured_size_field['choices'][ $featured_size_value ];
+      echo $featured_size_label;
+    } else {
+      echo 'Default (6 col)';
+    }
+  }
+}
+
+function my_column_init() {
+  echo get_current_screen();
+  add_filter( 'manage_post_posts_columns' , 'add_acf_columns' );
+  add_action('manage_post_posts_custom_column', 'featured_size_column', 10, 2);
+}
+add_action( 'admin_init' , 'my_column_init' );
+
+// --- TAX
+// Register Custom Taxonomy ANNO
+function project_year_taxonomy() {
+
+  $labels = array(
+    'name'                       => _x( 'Year', 'Taxonomy General Name', 'text_domain' ),
+    'singular_name'              => _x( 'Year', 'Taxonomy Singular Name', 'text_domain' ),
+    'menu_name'                  => __( 'Years', 'text_domain' ),
+    'all_items'                  => __( 'All Items', 'text_domain' ),
+    'parent_item'                => __( 'Parent Item', 'text_domain' ),
+    'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+    'new_item_name'              => __( 'New Item Name', 'text_domain' ),
+    'add_new_item'               => __( 'Add New Item', 'text_domain' ),
+    'edit_item'                  => __( 'Edit Item', 'text_domain' ),
+    'update_item'                => __( 'Update Item', 'text_domain' ),
+    'view_item'                  => __( 'View Item', 'text_domain' ),
+    'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+    'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+    'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+    'popular_items'              => __( 'Popular Items', 'text_domain' ),
+    'search_items'               => __( 'Search Items', 'text_domain' ),
+    'not_found'                  => __( 'Not Found', 'text_domain' ),
+    'no_terms'                   => __( 'No items', 'text_domain' ),
+    'items_list'                 => __( 'Items list', 'text_domain' ),
+    'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+  );
+  $args = array(
+    'labels'                     => $labels,
+    'hierarchical'               => true,
+    'public'                     => true,
+    'show_ui'                    => true,
+    'show_admin_column'          => true,
+    'show_in_nav_menus'          => true,
+    'show_tagcloud'              => true,
+    'show_in_rest'               => true,
+  );
+  register_taxonomy( 'project_year', array( 'post' ), $args );
+}
+add_action( 'init', 'project_year_taxonomy', 0 );
+
+// Register Custom Taxonomy ANNO
+function project_client_taxonomy() {
+
+  $labels = array(
+    'name'                       => _x( 'Client', 'Taxonomy General Name', 'text_domain' ),
+    'singular_name'              => _x( 'Client', 'Taxonomy Singular Name', 'text_domain' ),
+    'menu_name'                  => __( 'Clients', 'text_domain' ),
+    'all_items'                  => __( 'All Items', 'text_domain' ),
+    'parent_item'                => __( 'Parent Item', 'text_domain' ),
+    'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+    'new_item_name'              => __( 'New Item Name', 'text_domain' ),
+    'add_new_item'               => __( 'Add New Item', 'text_domain' ),
+    'edit_item'                  => __( 'Edit Item', 'text_domain' ),
+    'update_item'                => __( 'Update Item', 'text_domain' ),
+    'view_item'                  => __( 'View Item', 'text_domain' ),
+    'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+    'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+    'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+    'popular_items'              => __( 'Popular Items', 'text_domain' ),
+    'search_items'               => __( 'Search Items', 'text_domain' ),
+    'not_found'                  => __( 'Not Found', 'text_domain' ),
+    'no_terms'                   => __( 'No items', 'text_domain' ),
+    'items_list'                 => __( 'Items list', 'text_domain' ),
+    'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+  );
+  $args = array(
+    'labels'                     => $labels,
+    'hierarchical'               => true,
+    'public'                     => true,
+    'show_ui'                    => true,
+    'show_admin_column'          => true,
+    'show_in_nav_menus'          => true,
+    'show_tagcloud'              => true,
+    'show_in_rest'               => true,
+  );
+  register_taxonomy( 'project_client', array( 'post' ), $args );
+}
+add_action( 'init', 'project_client_taxonomy', 0 );
+
+// add_action('admin_head', 'my_custom_wysiwyg');
+// function my_custom_wysiwyg() {
+//   echo '<style>
+//     .acf-editor-wrap iframe {
+//       min-height: 100px !important;
+//       height: 100px;
+//     } 
+//     .acf-gallery {
+//     	height: 200px !important;
+//     }
+//     .wp-editor-area {
+//     	height: 70px !important;
+//     }
+//   </style>';
+// }
 
 
 ?>
