8a5a6f19b4bf4539609b33820bc7cac0985b4d74
Ilaria Roglieri <ilariaroglieri@gmail.com>
2026-02-24 16:19:37 +0100

    setting up flexible content: DONE

--- single.php
+++ single.php
@@ -11,7 +11,7 @@
 
         if ($medium_id):
       ?>
-        <section id="hero-section" class="container spacing-b-1">
+        <section id="hero-section" class="container spacing-b-2">
           <div class="d-flex flex-row">
             <div class="d-whole">
               <h1 class="s-medium spacing-b-half"><?php the_title(); ?></h1>
@@ -22,7 +22,7 @@
 
       <?php endif; ?>
 
-      <section id="text-section" class="container">
+      <section id="text-section" class="container spacing-t-2 spacing-b-3">
         <div class="d-flex flex-row m-column">
           <div class="d-7-twelfth m-whole">
             <div class="wysiwyg s-regular">
@@ -69,7 +69,7 @@
       <!-- flexible acf -->
       <?php
       if( have_rows('project_modules') ): while ( have_rows('project_modules') ) : the_row();
-        $bndCol = get_sub_field('background_color'); 
+        $bndCol = get_sub_field('dark_background');
         $moduleType = match (get_row_layout()) {
           'text_row' => 'text-module',
           'media_text_row' => 'media-text-module',
@@ -77,7 +77,7 @@
         };
         ?>
 
-        <div data-type="<?= $moduleType; ?>" class="container-fluid content-module spacing-p-t-3 spacing-p-b-3<?php if ($bndCol): ?> dark<?php endif; ?>">
+        <div data-type="<?= $moduleType; ?>" class="container-fluid content-module spacing-p-t-3 spacing-p-b-3<?php if ($bndCol == 1): ?> dark<?php endif; ?>">
           <div class="container">
             <?php if( get_row_layout() == 'text_row' ):
               $text = get_sub_field('text');
@@ -109,6 +109,49 @@
                 </div>
               </div>
 
+            <?php elseif( get_row_layout() == 'media_row' ): 
+              $media = get_sub_field('all_row_media');
+              $alignment = get_sub_field('media_alignment');
+              $twoSizes = get_sub_field('two_media_sizes');
+              $singleSize = get_sub_field('one_media_size');
+              $count = count($media);
+              ?>
+
+              <div class="d-flex flex-row <?= $alignment; ?> m-column">
+                <?php foreach ($media as $index => $m):
+                  // DEFAULT
+                  $class = 'd-half';
+                  $imgSize = 6;
+
+                  if ($count === 1) {
+                    $class = $singleSize;
+                    $imgSize = 12;
+
+                  } elseif ($count === 2) {
+                    if ($twoSizes === 'd-half') {
+                      $class = 'd-half';
+                      $imgSize = 6;
+                    } elseif ($twoSizes === 'd-one-third') {
+                      $class = ($index === 0) ? 'd-5-twelfth' : 'd-7-twelfth';
+                      $imgSize = ($index === 0) ? 4 : 6;
+                    } elseif ($twoSizes === 'd-two-thirds') {
+                      $class = ($index === 0) ? 'd-7-twelfth' : 'd-5-twelfth';
+                      $imgSize = ($index === 0) ? 6 : 4;
+                    }
+                  } elseif ($count === 3) {
+                    $class = 'd-one-third';
+                    $imgSize = 4;
+                  }
+
+                  $medium_id = get_medium_id_from_acf($m);
+                ?>
+
+                <div class="<?= $class; ?> m-whole">
+                  <?php render_media($medium_id, $imgSize); ?>
+                </div>
+
+                <?php endforeach; ?>
+              </div>
             <?php endif; ?>
           </div>
         </div>
