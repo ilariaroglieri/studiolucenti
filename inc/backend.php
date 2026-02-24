89558259fe1d269c1824187f122b4d20ceb8a5d3
Ilaria Roglieri <ilariaroglieri@gmail.com>
2026-02-24 16:41:22 +0100

    working on home & archive

--- inc/backend.php
+++ inc/backend.php
@@ -1,10 +1,35 @@
 <?php 
 
-add_filter('acf/load_field/name=original_width', function($field) {
-    $field['readonly'] = 1;
-    return $field;
-});
+// add acf width column to projects admin page
+function add_acf_columns($columns) {
+  $columns['size'] = 'Display size';
+  return $columns;
+}
 
+function size_info_column($column, $post_id) {
+  if ( $column == 'size' ) {
+    // Get the field object to fetch choices
+    $size = get_field_object('featured_medium_size', $post_id);
+    $value = get_field('featured_medium_size');;
+    $label = $size['choices'][ $value ];
+
+    if ($size) {
+      echo $label;
+    } else {
+      echo 'Default (6 col)';  // Or whatever you'd like to display when the field is empty
+    }
+  }
+}
+
+function my_column_init() {
+  echo get_current_screen();
+  add_filter( 'manage_post_posts_columns' , 'add_acf_columns' );
+  add_action('manage_post_posts_custom_column', 'size_info_column', 10, 2);
+}
+add_action( 'admin_init' , 'my_column_init' );
+
+
+// NON FUNZIONA AL MOMENTO
 add_filter('acf/prepare_field/name=featured_projects', function($field) {
 
   $post_id = acf_get_form_data('post_id'); // ID della pagina corrente
@@ -38,22 +63,13 @@ add_filter('acf/prepare_field/name=featured_projects', function($field) {
   return $field;
 });
 
-
-
-// add_action('admin_head', 'my_custom_wysiwyg');
-// function my_custom_wysiwyg() {
-//   echo '<style>
-//     .acf-editor-wrap iframe {
-//       min-height: 100px !important;
-//       height: 100px;
-//     } 
-//     .acf-gallery {
-//     	height: 200px !important;
-//     }
-//     .wp-editor-area {
-//     	height: 70px !important;
-//     }
-//   </style>';
-// }
+add_action('acf/input/admin_head', function() {
+  echo '<style>
+    .acf-gallery .acf-gallery-uploader {
+      max-height: 200px; /* Altezza massima per tutte le gallery */
+      overflow-y: auto;  /* Scroll se ci sono più elementi */
+    }
+  </style>';
+});
 
 ?>
