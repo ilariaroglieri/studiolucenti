6cc2d12bf2a6db47df1a84a141dc3de105f57d72
Ilaria Roglieri <ilariaroglieri@gmail.com>
2026-03-19 16:17:34 +0100

    backend showing original value

--- inc/backend.php
+++ inc/backend.php
@@ -29,39 +29,39 @@ function my_column_init() {
 add_action( 'admin_init' , 'my_column_init' );
 
 
-// NON FUNZIONA AL MOMENTO
-add_filter('acf/prepare_field/name=featured_projects', function($field) {
-
-  $post_id = acf_get_form_data('post_id'); // ID della pagina corrente
-  if (!$post_id) return $field;
-
-
-  // Recupera tutte le righe del repeater
-  $rows = get_field('featured_projects', $post_id);
-  if (empty($rows) || !is_array($rows)) return $field;
-
-  // print "<pre>";
-  // print_r($rows);
-  // print "</pre>";
-
-
-  foreach ($rows as $index => $row) {
-
-    if (empty($row['featured_project'])) continue;
-
-    $related_post = $row['featured_project'];
-    $rp_ID = $related_post->ID;
-
-    $original_value = get_field('featured_medium_size', $rp_ID);
-    var_dump($original_value);  // restitusice i giusti valori
-
-
-    $row['original_width'] = $original_value;
-
-  }
-
+add_filter('acf/prepare_field/name=original_width', function( $field ) {
+  $field['readonly'] = 1;
   return $field;
 });
+
+add_filter('acf/load_value/name=original_width', function ($value, $post_id, $field) { 
+
+  if (!$post_id) return $value; 
+
+  // extract repeater row index 
+  if (!preg_match('/featured_projects_(\d+)_original_width/', $field['name'], $matches)) { 
+    return $value; 
+  } 
+
+  $row_index = $matches[1]; 
+
+  // get the featured project selected in this row 
+  $featured_project_id = get_post_meta( 
+    $post_id, 
+    "featured_projects_{$row_index}_featured_project", 
+    true 
+  ); 
+
+  if (!$featured_project_id) return $value; 
+
+  // retrieve the value from the related post 
+  $originalSize = get_field_object('featured_medium_size', $featured_project_id);
+  $originalValue = get_field('featured_medium_size', $featured_project_id);
+  $originalLabel = $originalSize['choices'][ $originalValue ];
+
+  return $originalLabel; 
+
+}, 10, 3);
 
 add_action('acf/input/admin_head', function() {
   echo '<style>
